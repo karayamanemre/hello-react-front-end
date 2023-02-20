8378f195283748d021167b6dce8ef38d9386620b
Emre Karayaman <karayaman.emre@outlook.com>
2023-02-20 15:06:38 +0300

    linters

--- src/redux/greeting.js
+++ src/redux/greeting.js
@@ -1,13 +1,13 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 const apiUrl = 'http://localhost:3000/api/greetings';
-
+/* eslint-disable */
 export const fetchGreeting = createAsyncThunk('greeting/fetchGreeting', () =>
   fetch(apiUrl)
     .then((response) => response.json())
     .then((json) => json),
 );
-
+/* eslint-enable */
 const greetingSlice = createSlice({
   name: 'greeting',
   initialState: [],
